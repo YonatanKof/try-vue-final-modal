90c634cfa545f5d9c8e7e9877a0b806a767a620a
Yonatan Kof <kof@yonatankof.com>
2023-12-27 14:22:38 +0200

    Add vue-final-modal

--- app.vue
+++ app.vue
@@ -1,9 +1,14 @@
+<script setup lang="ts">
+import { ModalsContainer } from 'vue-final-modal';
+</script>
+
 <template>
 	<main>
 		<header>
 			<h3>Default Layout Header</h3>
 		</header>
 		<NuxtPage />
+		<ModalsContainer />
 		<footer>
 			<h3>Default Layout Footer</h3>
 		</footer>
@@ -11,14 +16,21 @@
 </template>
 
 <style>
-body{
-  font-family:system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+body {
+	font-family: system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans',
+		'Helvetica Neue', sans-serif;
 }
 main {
-  margin: 0 auto;
-  display: grid;
-  grid-template-rows: auto 1fr auto;
-  min-height: 100vh;
-  max-width: 800px;
+	margin: 0 auto;
+	display: grid;
+	grid-template-rows: auto 1fr auto;
+	min-height: 100vh;
+	max-width: 800px;
+}
+button {
+	padding: 0.5em 0.75em;
+	text-align: center;
+	text-decoration: none;
+	border-radius: 0.25em;
 }
 </style>
---
+++ pages/Preview.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+import { useModal } from 'vue-final-modal';
+import ModalConfirmPlainCss from '~/components/ModalConfirmPlainCss.vue';
+
+const { open, close } = useModal({
+	component: ModalConfirmPlainCss,
+	attrs: {
+		name: 'Hard no!',
+		onConfirm() {
+			close();
+		},
+	},
+	slots: {
+		default: '<p>The content of the modal</p>',
+	},
+});
+</script>
+
+<template>
+	<section>
+		<h1>Preview</h1>
+		<button @click="open">Open Modal</button>
+	</section>
+</template>
